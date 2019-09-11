100ce849d10f152e8ebb09785198bd6b87189e60
Gregor Cerar <grega90@gmail.com>
2019-09-11 07:50:15 +0200

    Initial commit for VESNA platform

---
+++ arch/platform/vesna/dev/at86rf2xx/rf2xx_arch.h
@@ -0,0 +1,63 @@
+#ifndef RF2XX_ARCH_H_
+#define RF2XX_ARCH_H_
+
+#include <stdint.h>
+
+#define RF2XX_MAX_FRAME_SIZE	(127)
+#define RF2XX_MIN_FRAME_SIZE	(3)
+#define RF2XX_CRC_SIZE			(2)
+#define RF2XX_LQI_SIZE			(1)
+#define RF2XX_MAX_PAYLOAD_SIZE	(RF2XX_MAX_FRAME_SIZE - RF2XX_CRC_SIZE)
+
+
+// the delay between radio Tx request and SFD sent, in rtimer ticks
+// This includes (FORCE_TRX_OFF, TX_ARET/PLL_ON, delay, PREAMBLE+SFD (p.39))
+#define RF2XX_DELAY_BEFORE_TX		((unsigned)US_TO_RTIMERTICKS(200))
+// * FORCE_TRX_OFF        - 1us
+// * TRX_OFF -> PLL_ON    - 110us
+// * PLL_ON -> BUSY_RX    - 16us
+// * PREAMBLE + SFD       - 160us (96us)
+//                        = 223us
+
+// the delay between radio Rx request and start listening, in rtimer ticks
+// (we need time from end of Tx until it reaches Rx mode again)
+#define RF2XX_DELAY_BEFORE_RX		((unsigned)US_TO_RTIMERTICKS(120))
+// * PLL_ON -> RX_ON      - 1us
+// * Time until PLL_LOCK                  // Even if the registerbits TRX_STATUS indicates RX_ON
+//   should occur         - 32us          // actual frame reception canonly start once the PLL has locked.
+// * PLL settling time                    
+//   on chanel switch     - (11us)          
+// * TRX_OFF -> RX_ON     - 110us         // when rf2xx_on is called
+//                        = 114us
+
+// The number of header and footer bytes of overhead at the PHY layer after SFD
+#define RF2XX_PHY_OVERHEAD			(3) 
+// * 1 length byte + 2 bytes CRC
+
+// the delay between the end of SFD reception and the radio returning 1 to receiving_packet()
+#define RF2XX_DELAY_BEFORE_DETECT	((unsigned)US_TO_RTIMERTICKS(40))
+// * RX_START flag
+//   interrupt latency    - 9us
+// * PHR reception        - 32us
+
+
+#define RF2XX_BYTE_AIR_TIME			(32) // us for 1 byte (@250kbps)
+// 1 / (250 kbps / 8) == 32 us/byte
+
+
+// The drift compared to "true" 10ms slots. --> Enable adaptive sync to enable compensation for this (enabled by default).
+#define RF2XX_BASE_DRIFT_PPM        (RTIMER_ARCH_PPM)
+// Slot length 10000 usec, 
+// which gives us 655 ticks (from macro US_TO_RTIMERTICKS)
+// Tick duration 15.2587890625 usec
+//             
+// Real slot duration is then : 9994.506835937 usec
+// Target - Real duration = 5.49316 usec
+// TSCH_CONF_BASE_DRIFT_PPM 549
+
+extern const uint16_t tsch_timeslot_timing_rf2xx_10000us_250kbps[];
+
+// TSCH timeslot timing (default is: 10ms tsch_timeslot_timing_us_10000)
+#define RF2XX_CONF_DEFAULT_TIMESLOT_TIMING	(tsch_timeslot_timing_rf2xx_10000us_250kbps)
+
+#endif
---
+++ arch/platform/vesna/dev/rtimer-arch.h
@@ -0,0 +1,35 @@
+#ifndef RTIMER_ARCH_H_
+#define RTIMER_ARCH_H_
+
+#include "sys/rtimer.h"
+
+#if (AT86RF2XX_BOARD_ISMTV_V1_0 || AT86RF2XX_BOARD_ISMTV_V1_1)
+    #define RTIMER_ARCH_SECOND      (65533)
+    #define RTIMER_ARCH_PPM         (594) // +1500 //TODO fix it if we have external timer source
+#else
+    #define RTIMER_ARCH_SECOND		(65503)
+    #define RTIMER_ARCH_PPM         (594) 
+#endif
+
+
+// Converts micro seconds to rtimer ticks (65536 ticks/s ---> 1us = 0.065536 tick)
+// Eqn.: T = (us * 0.065536) +- 1/2
+#define US_TO_RTIMERTICKS(us)   ((us) >= 0 ? \
+                                (uint32_t)(((((int64_t)(us)) * (RTIMER_ARCH_SECOND)) + 500000) / 1000000L) : \
+                                (uint32_t)(((((int64_t)(us)) * (RTIMER_ARCH_SECOND)) - 500000) / 1000000L)) 
+
+// Converts rtimer ticks to micro seconds (1/65536 Hz ---> 1 tick ~ 15.25879 us)
+// Eqn.: us = (T * 15.258) +- 1/2
+#define RTIMERTICKS_TO_US(t)    ((t) >= 0? \
+                                ((((int32_t)(t)) * 1000000L + ((RTIMER_ARCH_SECOND) / 2)) / (RTIMER_ARCH_SECOND)) : \
+                                ((((int32_t)(t)) * 1000000L - ((RTIMER_ARCH_SECOND) / 2)) / (RTIMER_ARCH_SECOND)))
+
+// Converts rtimer ticks to micro-seconds (64-bit version) because the 32-bit one cannot handle T >= 4294 ticks.
+// Intended only for positive values of t
+#define RTIMERTICKS_TO_US_64(t) ((uint32_t)(((int64_t)(t)) * 1000000L + ((RTIMER_ARCH_SECOND) / 2)) / (RTIMER_ARCH_SECOND)) 
+
+
+void contiki_rtimer_isr(void);
+rtimer_clock_t rtimer_arch_now(void);
+
+#endif
