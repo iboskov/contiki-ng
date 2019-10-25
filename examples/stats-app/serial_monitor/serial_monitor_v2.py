28117c2d8578dff8da1c98b7700341c9c9ff097f
Ciuc <gregaa.morano@gmail.com>
2019-10-25 15:16:07 +0200

    different approach for starting the app

---
+++ examples/stats-app/serial_monitor/serial_monitor_v2.py
@@ -0,0 +1,169 @@
+# Monitor serial input from given port and store it into given file
+
+import sys
+import argparse
+import serial
+from datetime import datetime
+
+LINES_TO_READ = 7000
+
+DEFAULT_FILE_NAME = "rf2xx_stats.txt"
+
+baseport = "/dev/ttyUSB"
+BAUD = 112500
+PARITY = serial.PARITY_NONE
+STOPBIT = serial.STOPBITS_ONE
+BYTESIZE = serial.EIGHTBITS
+
+# ----------------------------------------------------------------------
+# Argument parser for selection output text file - where to store data
+# ----------------------------------------------------------------------
+parser = argparse.ArgumentParser(
+    description="Store serial input into given file.",
+    formatter_class=argparse.MetavarTypeHelpFormatter
+)
+parser.add_argument("-o", 
+                    "--output", 
+                    help="select file to store serial input", 
+                    type=str,
+                    required=False)
+parser.add_argument("-p", 
+                    "--port",   
+                    help="""select serial port [ttyUSBx]...if no port 
+                    given, program will find it automaticly""",
+                    type=str, 
+                    required=False)
+parser.add_argument("-s", 
+                    "--skip",   
+                    help="skip waiting for the start sequence", 
+                    action="store_true")
+parser.add_argument("-r",
+                    "--root",
+                    help="set device as root of the network",
+                    action="store_true")
+
+args = parser.parse_args()
+
+# ----------------------------------------------------------------------
+# Open serial monitor
+# ----------------------------------------------------------------------
+if(not args.port):
+    # Find port automaticly - search for ttyUSB
+    for i in range(0, 12):
+        try:
+            port = baseport + str(i)
+            ser = serial.Serial(port, BAUD, BYTESIZE, PARITY, STOPBIT)
+            print("Serial monitor opened on port: " + port)
+            break
+        except:
+            print("No serial port connected or all in use!..Exiting now")
+            sys.exit(1)
+else:
+    # Connect to given port
+    try:
+        port = "/dev/" + args.port
+        ser = serial.Serial(port, BAUD, BYTESIZE, PARITY, STOPBIT)
+        print("Serial monitor opened on port: " + port)
+    except:
+        print("Serial port not connected or in use!..Exiting now")
+        sys.exit(1)
+
+
+# ----------------------------------------------------------------------
+# Prepare output file
+# ----------------------------------------------------------------------
+if(not args.output):
+    filename = DEFAULT_FILE_NAME
+    print("Storing into default file: " + filename)
+else:
+    filename = args.output
+    print("Storing into: " + filename)
+
+# (optional) Write first lines into it
+file = open(filename, mode="w", encoding="UTF-8")
+file.write(str(datetime.now())+"\n")
+file.write("----------------------------------------------------------------------------------------------- \n")
+file.write("Serial input from port:" + port + "\n")
+if(args.root):
+    file.write("Device is root of the DAG network! \n")
+file.write("----------------------------------------------------------------------------------------------- \n")
+file.close()
+
+# ----------------------------------------------------------------------
+# Set device as root of the network via serial CLI
+# ----------------------------------------------------------------------
+if(args.root):
+    print("Set device as DAG root")
+    try:
+        ser.write("rpl-set-root 1 \n".encode("ASCII"))
+    except:
+        print("Error writing to device!")
+    
+# ----------------------------------------------------------------------
+# Send start sequence '*'
+# ----------------------------------------------------------------------
+print("Send start sequence")
+try:
+    ser.write("***Start the app***\n".encode("ASCII"))
+except:
+    print("Error writing to device!")
+
+# ----------------------------------------------------------------------
+# Wait for response (again '*' character)
+# ----------------------------------------------------------------------
+print("Waiting for response")
+while(True):
+    try:
+        value = ser.readline()
+        if(chr(value[0]) == '*'):
+            break
+    except KeyboardInterrupt:
+        print("\n Keyboard interrupt!..Exiting now")
+        sys.exit(1)
+
+
+print("Start logging serial input") 
+
+# Open file to append serial input to it
+file = open(filename, "a")
+
+# ----------------------------------------------------------------------
+# Read input lines while LINES_TO_READ or until stop sequence '='
+# ----------------------------------------------------------------------
+i = 1
+try:
+    while(i <= LINES_TO_READ):
+        # Read one line (until \n char)
+        value = ser.read_until(b'\n', None)
+
+        # --------------------------------------------------------------
+        # If stop sequence '=' found, exit monitor
+        # --------------------------------------------------------------
+        if(chr(value[0]) == '='):
+            print("Found stop sequence!..stored " + str(i) + " lines.")
+            break
+
+        # Store value into file
+        file.write("[" + str(datetime.now().time())+"]: ")
+        value= value.decode("UTF-8")
+        file.write(str(value))
+
+        # Update status line in terminal
+        print("Line " + str(i) +"/(" + str(LINES_TO_READ) +")", end="\r")
+        i += 1
+    
+    print("")
+    print("Done!..Exiting serial monitor")
+
+except KeyboardInterrupt:
+    print("\n Keyboard interrupt!..Exiting serial monitor")
+
+except serial.SerialException:
+    print("Error opening port!..Exiting serial monitor")
+
+except IOError:
+    print("\n Serial port disconnected!.. Exiting serial monitor")
+
+finally:
+    file.close()
+    ser.close()
---
+++ examples/stats-app/stats-app_v2.c
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 2006, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+#include "contiki.h"
+#include <stdio.h>
+#include "dev/serial-line.h"
+#include "arch/platform/vesna/dev/at86rf2xx/rf2xx.h"
+#include "arch/platform/vesna/dev/at86rf2xx/rf2xx_stats.h"
+
+#define SECOND 1000
+#define STATS_BG_NOISE_BUFF_CAPACITY 503
+
+uint32_t i = 0;
+
+void STATS_print_help(void);
+void STATS_input_command(char *data);
+
+/*---------------------------------------------------------------------------*/
+PROCESS(stats_process, "Stats app process");
+PROCESS(serial_input_process, "Serial input command");
+AUTOSTART_PROCESSES(&serial_input_process);
+/*---------------------------------------------------------------------------*/
+
+PROCESS_THREAD(serial_input_process, ev, data)
+{
+    PROCESS_BEGIN();
+
+    while(1){
+      PROCESS_WAIT_EVENT_UNTIL(
+        (ev == serial_line_event_message) && (data != NULL));
+      STATS_input_command(data);
+    }
+
+    PROCESS_END();
+}
+
+PROCESS_THREAD(stats_process, ev, data)
+{
+  static struct etimer timer;
+
+  PROCESS_BEGIN();
+
+  printf("*** Got start sequence --> starting app! \n");
+
+  etimer_set(&timer, 1);  //ms = 1, sec = 1000
+
+  while(1) {
+    i++;
+
+  #if RF2XX_CONF_STATS
+    // Optional: print help into log file
+    if(i == 10){
+      STATS_print_help();
+    }
+
+    // Every 1ms measure rssi and store it into buffer
+    STATS_update_background_noise(STATS_BG_NOISE_BUFF_CAPACITY);
+  
+    // Every half second print measured values and clear the buffer
+    if(i%(SECOND/2) == 0) {
+      STATS_print_background_noise();
+    }
+
+    // Every 10 seconds print packet statistics and clear the buffer
+    if((i%(SECOND * 10)) == 0){
+      STATS_print_packet_stats();
+    }
+
+    // After 10 min send stop sequence ('=') and print driver statistics
+    if(i == (SECOND * 600)){
+      STATS_print_driver_stats();
+      printf("===== Stop monitoring serial port =====\n");
+    }
+  #endif
+
+    /* Wait for the periodic timer to expire and then restart the timer. */
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+    etimer_reset(&timer);
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+
+void
+STATS_input_command(char *data){
+        char cmd = data[0];
+        switch(cmd){
+          case '*':
+            process_start(&stats_process, NULL);
+            break;
+          
+          case 'r':
+            // rpl-set-root 1
+            break;
+        }
+}
+
+void
+STATS_print_help(void){
+  uint8_t addr[8];
+
+  rf2xx_driver.get_object(RADIO_PARAM_64BIT_ADDR, &addr, 8);
+
+  printf("\n"); 
+  printf("Device ID: ");
+  for(int j=0; j<8; j++){
+    printf("%X",addr[j]);
+  }
+  printf("\n"); 
+  printf("----------------------------------------------------------------------------\n");
+  printf("\n");
+  printf("       DESCRIPTION\n");
+  printf("----------------------------------------------------------------------------\n");
+  printf("CH[cc][xxx]([ss:us]): (tt)[v] (tt)[v] (tt)[v] ...\n");
+
+  printf("cc   - Channel number, where RSSI is measured \n");
+  printf("xxx  - Count of measured values \n");
+  printf("ss   - Timestamp in seconds \n");
+  printf("us   - Timestamp in micro seconds \n");
+  printf("tt   - Deviation from first measurment in micro seconds\n");
+  printf("v    - Measured RSSI values at theirs timestamp \n");
+  printf("----------------------------------------------------------------------------\n");
+  printf("T[n] [u] [t] [0xaaaa] \n");
+  printf("[s]:[us]\n");
+  printf("C[cc] L[ll] S[ss] | P[pp] \n");
+
+  printf("n    - Transmited packt count \n");
+  printf("u    - (1/0 = unicast/broadcast) \n");
+  printf("t    - Type of packet (D/B/A = data/beacon/ACK) \n");
+  printf("aaaa - Destination address \n");
+  printf("s    - Timestamp in seconds \n");
+  printf("us   - Timestamp in micro seconds \n");
+  printf("cc   - Channel number \n");
+  printf("ll   - Packet length in bytes \n");
+  printf("ss   - Sequence number \n");
+  printf("pp   - Transmision power (0 = 3dBm)\n");
+  printf("----------------------------------------------------------------------------\n");
+  printf("R[n] [t] [0xaaaa] \n");
+  printf("[s]:[us]\n");
+  printf("C[cc] L[ll] S[ss] | R[rr] Q[qq] \n");
+
+  printf("n    - Received packt count \n");
+  printf("t    - Type of packet (D/B/A = data/beacon/ACK) \n");
+  printf("aaaa - Source address \n");
+  printf("s    - Timestamp in seconds \n");
+  printf("us   - Timestamp in micro seconds \n");
+  printf("cc   - Channel number \n");
+  printf("ll   - Packet length in bytes \n");
+  printf("ss   - Sequence number \n");
+  printf("rr   - RSSI when packet was received\n");
+  printf("qq   - LQI when packet was received \n");
+  printf("----------------------------------------------------------------------------\n");
+  printf("\n");
+  printf("On the end of file, there is a count of all received and transmited packets. \n");
+  printf("----------------------------------------------------------------------------\n");
+}
+
+
